4ad2019f347e4a8d1fe9f84d945e79a82b2d3e75
Jonas Frey <dev@jonasfrey.de>
2022-11-07 12:20:25 +0100

    Initial Commit

---
+++ FilamentSwatches/FilamentSwatchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilamentSwatchesApp.swift
+//  FilamentSwatches
+//
+//  Created by Jonas Frey on 07.11.22.
+//
+
+import SwiftUI
+
+@main
+struct FilamentSwatchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
