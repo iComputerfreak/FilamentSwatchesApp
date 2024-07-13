b5168a458f93876507c11c4f310b445127bdfa79
Jonas Frey <dev@jonasfrey.de>
2024-07-13 13:50:40 +0200

    Add a dismiss button to the swatch view

--- FilamentSwatches/Views/SwatchView.swift
+++ FilamentSwatches/Views/SwatchView.swift
@@ -9,46 +9,61 @@ import SwiftUI
 
 struct SwatchView: View {
     let swatch: Swatch
+    @Environment(\.dismiss) private var dismiss
     @EnvironmentObject private var userData: UserData
     
     var body: some View {
-        ZStack {
-            swatch.color?.color
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                // MARK: Card
+        NavigationStack {
+            ZStack {
+                swatch.color?.color
+                    .ignoresSafeArea()
+                
                 VStack {
-                    Text(swatch.descriptiveName)
-                        .font(.largeTitle.bold())
-                        .padding(.bottom)
-                    SwatchViewRow(key: "Material", value: swatch.material)
-                    SwatchViewRow(key: "Brand", value: swatch.brand)
-                    if !swatch.productLine.isEmpty {
-                        SwatchViewRow(key: "Product Line", value: swatch.productLine)
+                    // MARK: Card
+                    VStack {
+                        Text(swatch.descriptiveName)
+                            .font(.largeTitle.bold())
+                            .padding(.bottom)
+                        SwatchViewRow(key: "Material", value: swatch.material)
+                        SwatchViewRow(key: "Brand", value: swatch.brand)
+                        if !swatch.productLine.isEmpty {
+                            SwatchViewRow(key: "Product Line", value: swatch.productLine)
+                        }
+                        SwatchViewRow(key: "Color", value: swatch.colorName)
+                        if swatch.extruderTemp > 0 {
+                            SwatchViewRow(key: "Extruder Temp", value: "\(swatch.extruderTemp) °C")
+                        }
+                        if swatch.bedTemp > 0 {
+                            SwatchViewRow(key: "Bed Temp", value: "\(swatch.bedTemp) °C")
+                        }
                     }
-                    SwatchViewRow(key: "Color", value: swatch.colorName)
-                    if swatch.extruderTemp > 0 {
-                        SwatchViewRow(key: "Extruder Temp", value: "\(swatch.extruderTemp) °C")
-                    }
-                    if swatch.bedTemp > 0 {
-                        SwatchViewRow(key: "Bed Temp", value: "\(swatch.bedTemp) °C")
+                    .padding()
+                    .background(.regularMaterial, in: RoundedRectangle(cornerRadius: 20))
+                    .padding()
+                    
+                    // MARK: Write Button
+                    WriteNFCButton(swatch: swatch)
+                    
+                    // MARK: Add to Library Button
+                    if !userData.swatches.contains(where: { $0.id == swatch.id }) {
+                        Button("Add to Library") {
+                            userData.swatches.append(swatch)
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(.indigo)
                     }
                 }
-                .padding()
-                .background(.regularMaterial, in: RoundedRectangle(cornerRadius: 20))
-                .padding()
-                
-                // MARK: Write Button
-                WriteNFCButton(swatch: swatch)
-                
-                // MARK: Add to Library Button
-                if !userData.swatches.contains(where: { $0.id == swatch.id }) {
-                    Button("Add to Library") {
-                        userData.swatches.append(swatch)
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Done")
+                            .bold()
                     }
-                    .buttonStyle(.borderedProminent)
-                    .tint(.indigo)
                 }
             }
         }
