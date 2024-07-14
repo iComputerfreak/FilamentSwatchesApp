cd8d743ea9ebf4496f170ebe48cd6b772ee2aab8
Jonas Frey <dev@jonasfrey.de>
2024-07-14 10:55:24 +0200

    Add attributes to Swatch model

--- FilamentSwatches/Models/Swatch.swift
+++ FilamentSwatches/Models/Swatch.swift
@@ -15,8 +15,9 @@ struct Swatch: Identifiable, Codable, Hashable {
     var productLine: String
     var colorName: String
     var color: FilamentColor?
-    var extruderTemp: Int = 0
-    var bedTemp: Int = 0
+    var extruderTemp: Int
+    var bedTemp: Int
+    var attributes: [String: String]
     
     var descriptiveName: String {
         "\(productLine.isEmpty ? brand : productLine) \(colorName) \(material)"
@@ -29,7 +30,16 @@ struct Swatch: Identifiable, Codable, Hashable {
         !colorName.isEmpty
     }
     
-    init(material: String, brand: String, productLine: String = "", colorName: String, color: FilamentColor? = nil, extruderTemp: Int = 0, bedTemp: Int = 0) {
+    init(
+        material: String,
+        brand: String,
+        productLine: String = "",
+        colorName: String,
+        color: FilamentColor? = nil,
+        extruderTemp: Int = 0,
+        bedTemp: Int = 0,
+        attributes: [String: String] = [:]
+    ) {
         self.material = material
         self.brand = brand
         self.productLine = productLine
@@ -37,5 +47,20 @@ struct Swatch: Identifiable, Codable, Hashable {
         self.color = color
         self.extruderTemp = extruderTemp
         self.bedTemp = bedTemp
+        self.attributes = attributes
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(UUID.self, forKey: .id)
+        self.material = try container.decode(String.self, forKey: .material)
+        self.brand = try container.decode(String.self, forKey: .brand)
+        self.productLine = try container.decode(String.self, forKey: .productLine)
+        self.colorName = try container.decode(String.self, forKey: .colorName)
+        self.color = try container.decodeIfPresent(FilamentColor.self, forKey: .color)
+        self.extruderTemp = try container.decode(Int.self, forKey: .extruderTemp)
+        self.bedTemp = try container.decode(Int.self, forKey: .bedTemp)
+        // NOTE: attributes were added in a later version, so they might not be present
+        self.attributes = try container.decodeIfPresent([String: String].self, forKey: .attributes) ?? [:]
     }
 }
