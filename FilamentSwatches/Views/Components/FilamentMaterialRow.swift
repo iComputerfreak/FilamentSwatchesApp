d803545411e26c862f7650b6991a10bd94485d9e
Jonas Frey <dev@jonasfrey.de>
2024-07-20 19:58:34 +0200

    Rework FilamentMaterialRow

--- FilamentSwatches/Views/Components/FilamentMaterialRow.swift
+++ FilamentSwatches/Views/Components/FilamentMaterialRow.swift
@@ -5,18 +5,55 @@
 //  Created by Jonas Frey on 07.11.22.
 //
 
+import DependencyInjection
 import SwiftUI
 
 struct FilamentMaterialRow: View {
-    let material: String
+    private var material: FilamentMaterial
+    
+    @Injected private var userData: UserData
+    
+    // swiftlint:disable:next type_contents_order
+    init(material: FilamentMaterial) {
+        self.material = material
+    }
     
     var body: some View {
-        Text(material)
+        Text(material.name)
+            .swipeActions {
+                deleteButton
+                editButton
+            }
+            .contextMenu {
+                editButton
+                deleteButton
+            }
+    }
+    
+    @ViewBuilder
+    private var deleteButton: some View {
+        Button(role: .destructive) {
+            userData.materials.removeAll { other in
+                other.id == material.id
+            }
+            userData.save()
+        } label: {
+            Label("Delete", systemImage: "trash")
+        }
+    }
+    
+    @ViewBuilder
+    private var editButton: some View {
+        NavigationLink {
+            EditMaterialView(viewModel: .init(material: material))
+        } label: {
+            Label("Edit", systemImage: "pencil")
+        }
     }
 }
 
 struct FilamentMaterialRow_Previews: PreviewProvider {
     static var previews: some View {
-        FilamentMaterialRow(material: "PLA")
+        FilamentMaterialRow(material: .init(name: "PLA"))
     }
 }
