6d60defc5bb42f96cc313735790f95e107550466
Jonas Frey <dev@jonasfrey.de>
2024-07-20 19:58:13 +0200

    Make FilamentMaterial observable

--- FilamentSwatches/Model/Models/FilamentMaterial.swift
+++ FilamentSwatches/Model/Models/FilamentMaterial.swift
@@ -2,7 +2,34 @@
 
 import Foundation
 
-struct FilamentMaterial: Codable, Hashable, Identifiable {
+@Observable
+class FilamentMaterial: Codable, Hashable, Identifiable {
+    enum CodingKeys: String, CodingKey {
+        // swiftlint:disable identifier_name
+        case _id = "id"
+        case _name = "name"
+        // swiftlint:enable identifier_name
+    }
+    
     var id: UUID = .init()
     var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decodeIfPresent(UUID.self, forKey: ._id) ?? UUID()
+        self._name = try container.decode(String.self, forKey: ._name)
+    }
+    
+    static func == (lhs: FilamentMaterial, rhs: FilamentMaterial) -> Bool {
+        return lhs.id == rhs.id && lhs.name == rhs.name
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(name)
+    }
 }
